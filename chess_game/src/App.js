4e7ab9afbf6f6fe17c54b54fcca3c452e29fc34f
Carlos Alberto Ráxtum Ramos <rax19721@uvg.edu.gt>
2022-09-29 19:21:32 -0600

    download exe

--- chess_game/src/App.js
+++ chess_game/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Link } from 'react';
 
 function App() {
   return (
@@ -7,14 +7,14 @@ function App() {
       <div>
         Brisca Game
       </div>
-      <br/>
+      <br />
       <div>
         <p>
           Rules:
         </p>
         <p>
-          El orden, de mayor a menor, es el siguiente: 
-          as, tres, rey, caballo, sota, siete, seis, cinco, cuatro y dos. 
+          El orden, de mayor a menor, es el siguiente:
+          as, tres, rey, caballo, sota, siete, seis, cinco, cuatro y dos.
           El valor de las cartas, en cualquiera de los cuatro palos, es :
         </p>
         <ol>
@@ -36,10 +36,10 @@ function App() {
             El jugador que da las cartas, las barajará ofreciéndoselas a cortar al de su izquierda, quien al hacerlo, no podrá tomar ni dejar menos de cuatro cartas. Después, distribuirá tres cartas a cada jugador, de una en una, en sentido contrario a las agujas del reloj, descubriendo la carta siguiente, que es la que señala el palo de triunfo, y que quedará a la vista junto al mazo en el centro de la mesa.El turno de dar, en los juegos siguientes, continúa por orden riguroso de izquierda a derecha.
           </p>
         </div>
-        <br/>
+        <br />
         <div>
           <p>
-          Inicio y marcha del juego
+            Inicio y marcha del juego
           </p>
           <p>
             Inicia el juego el jugador situado a la derecha del dador, el “mano”, jugando una carta que dejará descubierta sobre la mesa. Los demás jugadores, al llegarles su turno, pueden jugar una carta cualquiera, sin obligación de asistir al palo ni jugar triunfo. Gana la baza la mayor carta jugada del palo de triunfo y, en su defecto, la carta más alta del palo de salida.
@@ -51,12 +51,18 @@ function App() {
 
         <div>
           <p>
-          Git
+            Git
           </p>
+          <a
+            href="../brisca_client/brisca.exe"
+            download
+          >
+            Click to download
+          </a>
           <ol>
-          <li><a href="https://github.com/Mar19845/chess_front" >FRONT END / CLIENT</a></li>
-          <li><a href="https://github.com/Mar19845/chess_backend" >BACK END</a></li>
-        </ol>
+            <li><a href="https://github.com/Mar19845/chess_front" >FRONT END / CLIENT</a></li>
+            <li><a href="https://github.com/Mar19845/chess_backend" >BACK END</a></li>
+          </ol>
         </div>
       </div>
     </div>
